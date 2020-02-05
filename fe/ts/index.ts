bed5e33487315f19101e62b485bb2e43c0e66b54
wbcs <421768544@qq.com>
2020-02-05 16:16:40 +0800

    feat: save

--- fe/ts/index.ts
+++ fe/ts/index.ts
@@ -1,16 +1,42 @@
-interface Action<T> {
-  payload?: T
-  type: string
+enum FooIdBrand {
+  _ = ''
+}
+type FooId = FooIdBrand & string;
+
+// BAR
+enum BarIdBrand {
+  _ = ''
+}
+type BarId = BarIdBrand & string;
+
+let fooId: FooId;
+let barId: BarId;
+
+// 类型安全
+fooId = barId; // error
+barId = fooId; // error
+
+// 创建一个新的
+fooId = 'foo' as FooId;
+barId = 'bar' as BarId;
+
+// 两种类型都与基础兼容
+let str: string;
+str = fooId;
+str = barId;
+
+import React from 'react'
+interface Shit<T , S> {
+  prop0: T
+  prop1: S
+}
+class Fuck<T, S> extends React.Component<Shit<T, S>> {
+  constructor(props) {
+    super(props)
+  }
 }
 
-interface Module {
-  count: number;
-  message: string;
-  asyncMethod<T, U>(input: Promise<T>): Promise<Action<U>>;
-  syncMethod<T, U>(action: Action<T>): Action<U>;
-}
-
-type Result = {
-  asyncMethod<T, U>(input: T): Action<U>;
-  syncMethod<T, U>(action: T): Action<U>;
-}
+const obj = new Fuck<string, number>({
+  prop0: '',
+  prop1: 0
+})
--- server/python/django/README.md
+++ server/python/django/README.md
@@ -57,4 +57,39 @@ path('fuck/', include([
 ```sh
 requirements.txt <-> package.json
 pip install -r requirements.txt <-> npm install
+npm init <-> jango-admin startproject projectname
+```
+
+# 一些命令
+```sh
+django-admin startproject project_name
+python manage.py runserver [ip:port]
+# 创建migration相关文件
+python manage.py makemigrations polls
+# 根据model生成对应的sql
+python manage.py sqlmigrate polls 0001
+# 执行迁移工作，根据migration激活
+python manage.py migrate
+```
+
+使用定义好的models：
+```sh
+# 不直接使用python，是因为这样可以根据manage.py中的setting文件
+# 根据设置的各种环境变量来引入对应的包，就不是直接全局的了
+python manage.py shell
+```
+比如现在定义了一个model：
+```py
+from django.db import models
+
+class Fuck(models.Model):
+  shit = models.CharFields(max_length=100)
+```
+
+```py
+obj = Fuck(shit='fuck you mother, bitch')
+Fuck.objects.all()
+obj.id # 主键
+obj.shit = 'fuck you again'
+obj.save()
 ```
